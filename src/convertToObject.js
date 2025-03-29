68136e78828546a5701577f222ab8421acbbb5bc
Viktor-1995 <madebyvityok@ukr.net>
2025-03-29 22:51:10 +0100

    Solution 1

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,6 +7,28 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  const result = {};
+  const separeteArray = sourceString.trim().split(';');
+  let key;
+  let value;
+
+  if (sourceString === '') {
+    return result;
+  }
+
+  for (const cssPair of separeteArray) {
+    if (cssPair.length > 1 && cssPair) {
+      const cssRule = cssPair.trim();
+
+      if (cssRule.length > 1) {
+        key = cssRule.split(':')[0];
+        value = cssRule.split(':')[1];
+        result[key.trim()] = value.trim();
+      }
+    }
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;
